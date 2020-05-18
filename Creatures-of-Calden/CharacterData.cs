1fef7afe88d9a69688c91deec71a927d4d5ba049
Dylan Dove <dylandc101@gmail.com>
2020-05-18 00:55:54 -0400

    add more to game and edited CharacterData

--- Creatures-of-Calden/CharacterData.cs
+++ Creatures-of-Calden/CharacterData.cs
@@ -8,6 +8,7 @@ namespace Creatures_of_Calden
     {
         public string Class;
         public int Level;
+        public string[] Equipped = new string[6];
         //create inventory array with max of 10 items
         public string[] Inventory = new string[10];
 
@@ -17,6 +18,18 @@ namespace Creatures_of_Calden
             if (Class == "wizard")
             {
                 Inventory = new string[] { "spell component pouch", "spellbook", "knife", "", "", "", "", "", "", "" };
+            }
+            if (Class == "rogue")
+            {
+                Inventory = new string[] { "knife", "knife", "crossbow", "", "", "", "", "", "", "", };
+            }
+            if (Class == "barbarian")
+            {
+                Inventory = new string[] { "axe", "wooden shield", "", "", "", "", "", "", "", "", };
+            }
+            if (Class == "fighter")
+            {
+                Inventory = new string[] { "longsword", "shortsword", "rusty armor", "", "", "", "", "", "", "", };
             }
         }
     }
--- Creatures-of-Calden/Program.cs
+++ Creatures-of-Calden/Program.cs
@@ -52,44 +52,44 @@ namespace Hello_World_2
             Console.WriteLine("Please choose a class for your adventure");
             Console.WriteLine("Your options are as follows:");
             Console.WriteLine("Rogue, Wizard, Barbarian, Fighter, Monk");
-            newCharacter.characterClass = UserInput();
+            newCharacter.Class = UserInput();
             
 
             //close application if user chooses monk
-            if (userClass == "monk")
+            if (newCharacter.Class == "monk")
             {
                 Console.WriteLine("Rocks Fall.  Everyone Dies.");
                 System.Environment.Exit(0);
                 Console.ReadLine();
             }
 
-            Console.WriteLine("You have chosen " + userClass);
+            Console.WriteLine("You have chosen " + newCharacter.Class);
 
-            if (userClass == "rogue")
+            if (newCharacter.Class == "rogue")
             {
                 Console.WriteLine("Rogues start with two knives and a crossbow.");
                 Console.WriteLine("Rogues can wear light or medium armor.");
                 Console.WriteLine("At first level, rogues can sneak attack and disengage. \n");
             }
 
-            if (userClass == "wizard")
+            if (newCharacter.Class == "wizard")
             {
                 Console.WriteLine("Wizards start with a pouch of spell components, a spellbook, and a knife.");
                 Console.WriteLine("Wizards are not permitted to wear any armor per the Rialto Concord.");
-                Console.WriteLine("Wizards know three basic spells at first level and learn more as they progress.");
-                Console.WriteLine("At first level, all wizards know the magic bolt, summon fire, and telekinesis spells.");
+                Console.WriteLine("Wizards know three basic spells at Level 1 and learn more as they progress.");
+                Console.WriteLine("At Level 1, all wizards know the magic bolt, summon fire, and telekinesis spells.");
                 Console.WriteLine("To access your spellbook, simply type \"spellbook\" at any time. \n");
 
             }
-            if (userClass == "barbarian")
+            if (newCharacter.Class == "barbarian")
             {
                 Console.WriteLine("Barbarians start with an axe and a wooden shield.");
                 Console.WriteLine("Barbarians are tough, and thus cannot wear armor, or their family will disown them.");
                 Console.WriteLine("At first level, barbarians can entle bloodlust twice a day. \n");
             }
-            if (userClass == "fighter")
+            if (newCharacter.Class == "fighter")
             {
-                Console.WriteLine("Fighters start with two shortswords and rusty armor.");
+                Console.WriteLine("Fighters start with a longsword, a shortsword, and rusty armor.");
                 Console.WriteLine("Fighters can wear any armor, as long they have a sufficient strength score. \n");
 
             }
@@ -97,6 +97,11 @@ namespace Hello_World_2
             Console.WriteLine("You find yourself awakening in an unfamiliar location.  You're in what appears to be a once-fine tavern that has gone to disrepair.  There are no patrons, and the fire is unlit. \n");
             Console.WriteLine("You are sitting hunched over in a corner table, with a decent view of the entire room.  You can see that the door is hanging off it's hinges and there's no propieter in sight. \n");
             Console.WriteLine("The whole room appears to be covered in a substantial layer of dust.  Oddly, you have what appears to be a clean glass full of ale on the table in front of you. \n");
+            Console.WriteLine("In this game, you will be presented with 3 options for each action you may take.  Remember to choose wisely.");
+            Console.WriteLine("You have three options you may take:  drink from the glass, ignore the glass, or inspect the glass.");
+
+
+            
 
             //This will make sure the app stays open
             Console.ReadLine();
